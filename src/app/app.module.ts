a6f3c1ee293a41c5a83a04c5552e4ed3bf5ab001
Jorge Mina <minajorge@hotmail.com>
2020-09-30 15:45:45 -0500

    feat: Login Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+// Modules
 import { AppRoutingModule } from './app-routing.module';
+import { PrimeNGModule } from './primeng.module';
+// Components
 import { AppComponent } from './app.component';
+// Providers
+import { authInterceptorProviders } from './helpers/auth.interceptor';
+import { LoginComponent } from './components/login/login.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, SidebarComponent, DashboardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PrimeNGModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [authInterceptorProviders],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/primeng.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// PrimeNG
+import { ChartModule } from 'primeng/chart';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { TableModule } from 'primeng/table';
+import { CheckboxModule } from 'primeng/checkbox';
+import { EditorModule } from 'primeng/editor';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+
+const myModule = [
+  ChartModule,
+  DialogModule,
+  ButtonModule,
+  MessagesModule,
+  MessageModule,
+  ToastModule,
+  TableModule,
+  CheckboxModule,
+  EditorModule,
+  ProgressSpinnerModule,
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, myModule],
+  exports: [myModule],
+})
+export class PrimeNGModule {}
